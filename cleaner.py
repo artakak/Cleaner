ab6fa01dd2ebafdffd0dabbe2d8349ea7fd67cae
artakak <a260641139@ya.ru>
2018-04-09 22:54:18 +0300

    dffdf

--- cleaner.py
+++ cleaner.py
@@ -4,6 +4,7 @@ import os
 import subprocess
 import time
 from Blynk import Blynk
+import re
 
 
 ip = "192.168.1.51"
@@ -14,8 +15,23 @@ os.environ["MIROBO_TOKEN"] = token
 
 start_button = Blynk(auth, pin="V5")
 stop_button = Blynk(auth, pin="V6")
-repeat = Blynk(auth, pin="V9")
+
+repeat_kor = Blynk(auth, pin="V9")
+power_kor = Blynk(auth, pin="V11")
+
+repeat_room1 = Blynk(auth, pin="V12")
+power_room1 = Blynk(auth, pin="V13")
+
+repeat_room2 = Blynk(auth, pin="V14")
+power_room2 = Blynk(auth, pin="V15")
+
+repeat_kitchen = Blynk(auth, pin="V16")
+power_kitchen = Blynk(auth, pin="V17")
+
 terminal = Blynk(auth, pin="V7")
+
+lcd1 = Blynk(auth, pin="V8")
+lcd2 = Blynk(auth, pin="V10")
 
 areas = {"V0": [21281, 24865, 24831, 26365, 1],
          "V1": [24850, 24765, 27350, 26165, 1],
@@ -30,13 +46,31 @@ def do_robo_cmd(cmd):
     return result.replace("\\", r"\\").replace("\n", r"\n")
 
 
+def update_app(status):
+    state = re.findall("State: ([\w\s]+)", status)[0]
+    battery = re.findall("Battery: (\d+ %)", status)[0]
+    cleaning_duration = re.findall("Cleaning since: (\d:\d\d:\d\d)", status)[0]
+    lcd1.set_val(state)
+    lcd2.set_val(battery)
+    if state == "Zoned cleaning":
+        lcd2.set_val(cleaning_duration)
+    return state
+
+
 while 1:
     if start_button.get_val()[0] == "1":
         if "Charging" in do_robo_cmd("mirobo status"):
             terminal.set_val(r'\n\nStart cleaning\n')
             for t in areas.keys():
                 button = Blynk(auth, pin=t)
-                areas[t][-1] = int(repeat.get_val()[0])
+                if t == "V0" or t == "V1":
+                    areas[t][-1] = int(repeat_kor.get_val()[0])
+                elif t == "V2":
+                    areas[t][-1] = int(repeat_room1.get_val()[0])
+                elif t == "V3":
+                    areas[t][-1] = int(repeat_room2.get_val()[0])
+                elif t == "V4":
+                    areas[t][-1] = int(repeat_kitchen.get_val()[0])
                 if button.get_val()[0] == "1":
                     areas_to_clean.append(areas[t])
             result = do_robo_cmd("mirobo raw_command app_zoned_clean '%s'" % str(areas_to_clean))
@@ -56,8 +90,11 @@ while 1:
         stop_button.off()
         time.sleep(5)
         terminal.set_val(do_robo_cmd("mirobo status"))
-    if "Zone" in do_robo_cmd("mirobo status"):
+    try:
         current_status = do_robo_cmd("mirobo status")
         print(current_status)
+        print (update_app(current_status))
+    except:
+        pass
     time.sleep(5)
 
